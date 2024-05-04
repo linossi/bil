8968f461008f7222f6d238372f49e56a657800f3
Linossi <145012929+linossi@users.noreply.github.com>
2024-05-04 16:24:33 +0200

    save

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Definerer funksjonen visValg
+function visValg() {
     let kinoEl = document.querySelector("#kino");
     let dvdEl = document.querySelector("#dvd");
     let strommetjenesteEl = document.querySelector("#strommetjeneste");
     let tvEl = document.querySelector("#tv");
     let resultatEl = document.querySelector("p");
-    let knappEl = document.querySelector("#knapp");
 
+    // Lager en tekst der vi bygger opp en enkel liste
+    let liste = "<ul>";
 
-    // Legger til en lytter på knappen
-    knappEl.addEventListener("click", visValg);
-    
-    // Funksjon som finner ut hvilke avkrysningsbokser som er valgt
-    function visValg() {
-      // Lager en tekst der vi bygger opp en enkel liste
-      liste = "<ul>";
-    
-        if (kinoEl.checked) { liste += "<li>Kino</li>"; }
-        if (dvdEl.checked) { liste += "<li>DVD</li>"; }
-        if (strommetjenesteEl.checked) { liste += "<li>Strømmetjeneste</li>"; }
-        if (tvEl.checked) { liste += "<li>TV</li>"; }
-    
-      liste += "</ul>";
-    
-      resultatEl.innerHTML = "Du har valgt " + liste;
-    }
-});
+    if (kinoEl.checked) { liste += "<li>Kino</li>"; }
+    if (dvdEl.checked) { liste += "<li>DVD</li>"; }
+    if (strommetjenesteEl.checked) { liste += "<li>Strømmetjeneste</li>"; }
+    if (tvEl.checked) { liste += "<li>TV</li>"; }
+
+    liste += "</ul>";
+
+    resultatEl.innerHTML = "Du har valgt " + liste;
+}
+
+// Legger til en lytter på knappen
+let knappEl = document.querySelector("#knapp");
+knappEl.addEventListener("click", visValg);
+
